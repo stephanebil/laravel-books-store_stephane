d56163dfb96e1c7ef7f79e7bf70ed2a6cfdf2394
stephanebil <stephanebilella@yahoo.fr>
2022-10-20 15:25:00 +0200

    laravel-books-store_stephane projet terminé

--- resources/views/components/btn-delete.blade.php
+++ resources/views/components/btn-delete.blade.php
@@ -3,6 +3,6 @@
         <form action="{{ route('books.destroy', $book->id) }}" method="POST" onsubmit="return confirm('Es-tu sûr de vouloir supprimer ce livre?')">
             @csrf
             @method('DELETE')
-            <button class="bg-red-500 text-white p-3 rounded" type="submit">Supprimer</button>
+            <button class="bg-red-500 text-white p-2 rounded" type="submit">Supprimer</button>
     </form>
 </div>
